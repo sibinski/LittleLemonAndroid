b3e4c2a73b9a632a679c3cd27e0a53163fcde768
Jevgeni Shibinski <jevgeni.shibinski@gmail.com>
2025-04-20 14:45:06 +0300

    Updated, added Network.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    kotlin("plugin.serialization") version "1.5.21"
+
 }
 
 android {
@@ -42,6 +44,7 @@ android {
 dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation ("androidx.navigation:navigation-compose:2.8.9")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.1.3") // You might have this already, but ensure it's there
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
---
+++ app/src/main/java/com/example/littlelemonandroid/Network.kt
@@ -0,0 +1,38 @@
+package com.example.littlelemonandroid
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MenuNetwork(
+    @SerialName("menu")
+    val menuItems: List<MenuItemNetwork>
+)
+
+@Serializable
+data class MenuItemNetwork(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val price: String,
+    val image: String,
+    val category: String
+) {
+    fun toMenuItemRoom() = MenuItemRoom(
+        id = id,
+        title = title,
+        price = price.toDouble(),
+        desc = description,
+        image = image,
+        category = category
+    )
+
+    data class MenuItemRoom(
+        val id: Int,
+        val title: String,
+        val price: Double,
+        val desc: String,
+        val image: String,
+        val category: String
+    )
+}
